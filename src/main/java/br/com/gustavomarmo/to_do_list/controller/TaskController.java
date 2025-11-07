80edeadf6f6cba7f19ccca1ffe65fdbfc0f7c47f
gustavomarmo <gsmarmo@gmail.com>
2025-11-07 10:54:29 -0300

    refact: alteração do nome da controller no Swagger UI

--- src/main/java/br/com/gustavomarmo/to_do_list/controller/TaskController.java
+++ src/main/java/br/com/gustavomarmo/to_do_list/controller/TaskController.java
@@ -10,6 +10,7 @@ import br.com.gustavomarmo.to_do_list.service.TaskService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(name = "Endpoints Tasks")
 public class TaskController {
 
     private final TaskService taskService;
